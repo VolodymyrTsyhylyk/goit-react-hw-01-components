778194eb112f1509b446b12a3e2376b6fb21c570
Volodymyr Tsyhylyk <82731850+VolodymyrTsyhylyk@users.noreply.github.com>
2021-12-19 12:33:36 +0200

    Final v 1.1.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,31 @@
 import React from "react";
+import Profile from "./components/Profile/Profile";
+import Statistics from "./components/Statistics/Statistics";
+import FriendList from "./components/FriendList/FriendList";
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
+
+import user from "./database/user.json";
+import data from "./database/data.json";
+import friends from "./database/friends.json";
+import transactions from "./database/transactions.json";
+
+export default function App() {
+  return (
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </div>
+  );
+}
 
 // -----------------------------------
 // import logo from './logo.svg';
